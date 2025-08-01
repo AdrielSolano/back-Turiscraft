791a7a5552a79c7bf14e00b144030d10226a6012
AdrielSolano <2302019@utrivieramaya.edu.mx>
2025-08-01 01:42:39 -0500

    f

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import app from './app.js';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`); 
 })
-
-// ✅ Esto es válido en ES Module:
-import path from "path";
 
 // Servir React desde Express (después de tus rutas API)
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
